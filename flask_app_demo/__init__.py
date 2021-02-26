bc6d00dd66c26c0dcd2a10c940392004870d2862
mal-admin <myautoloan-admin@myautoloan.com>
2021-02-25 20:03:52 -0700

    committing with index and 404 page, config file, external db access

--- flask_app_demo/__init__.py
+++ flask_app_demo/__init__.py
@@ -1,4 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 
 def create_app():
@@ -6,10 +9,14 @@ def create_app():
 
     app.config.from_pyfile('config.py')
 
-    print(app.config["TEST_VAR"]) # confirms that variables are being loaded
+    db.init_app(app)
 
     from flask_app_demo.main.routes import main
     app.register_blueprint(main)
+
+    @app.errorhandler(404)
+    def page_not_found(e):
+        return render_template('404.html'), 404
 
     return app
 
